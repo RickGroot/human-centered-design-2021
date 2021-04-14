fdafbb9c27240714817504428c79fbf3e463fa55
Rick Groot <rickgroot41@gmail.com>
2021-04-14 13:44:00 +0200

    added test buttons with different solutions

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -31,3 +31,54 @@ function addClass(item, name) {
         parent.classList.add('right')
     }
 }
+
+
+let btn1 = document.getElementById('option1')
+let btn2 = document.getElementById('option2')
+let btn3 = document.getElementById('option3')
+let btn4 = document.getElementById('option4')
+
+let homeLink = document.querySelector('#podcast>a')
+let aside = document.querySelector('#podcast>aside')
+
+btn1.addEventListener('click', e => {
+    e.preventDefault()
+    homeLink.classList.remove('class2')
+    aside.classList.remove('class2')
+})
+
+btn2.addEventListener('click', e => {
+    e.preventDefault()
+    homeLink.classList.add('class2')
+    aside.classList.add('class2')
+})
+
+btn3.addEventListener('click', e => {
+    e.preventDefault()
+
+    let left = document.querySelectorAll('.left')
+    let right = document.querySelectorAll('.right')
+    
+    left.forEach(cont => {
+        cont.classList.remove('left2')
+    })
+
+    right.forEach(cont => {
+        cont.classList.remove('right2')
+    })
+})
+
+btn4.addEventListener('click', e => {
+    e.preventDefault()
+
+    let left = document.querySelectorAll('.left')
+    let right = document.querySelectorAll('.right')
+    
+    left.forEach(cont => {
+        cont.classList.add('left2')
+    })
+
+    right.forEach(cont => {
+        cont.classList.add('right2')
+    })
+})
